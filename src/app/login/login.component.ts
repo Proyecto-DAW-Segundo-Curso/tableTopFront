447ef1ffbd2883477de717c88400acd405deb203
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-03-03 00:45:17 +0100

    Agregar formulario de inicio de sesión con validación y estilos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginForm: FormGroup;
+  errorMessage: string = '';
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    // Inicializa el formulario reactivo
+    this.loginForm = this.fb.group({
+      email: ['user@gmail.com', Validators.required],
+      password: ['user1234', Validators.required]
+    })
+  }
+
+  // Método para manejar el envío del formulario
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+
+      // Intenta autenticar al usuario
+      if (this.authService.login(email, password)) {
+        this.router.navigate(['/dashboard']); // Redirige al dashboard
+      } else {
+        this.errorMessage = 'Email o contraseña incorrectos';
+      }
+    } else {
+      this.errorMessage = 'Por favor, completa el formulario correctamente';
+    }
+  }
 }
