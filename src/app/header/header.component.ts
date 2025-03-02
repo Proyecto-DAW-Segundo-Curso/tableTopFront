e6a8a27c08a305367162207ecd30aca5f1f47fa5
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-03-03 00:48:27 +0100

    Agregar componente de encabezado con navegación y estilos

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  appName = 'Table Top'; // Nombre de la aplicación
+  isLoggedIn = false; // Estado de autenticación
+
+  constructor(private authService: AuthService, private router: Router) {
+    // Verifica si el usuario está logueado
+    this.isLoggedIn = this.authService.isAuthenticated();
+    console.log('Is logged in:', this.isLoggedIn);
+    
+  }
+
+  // Método para redirigir al login
+  redirectToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  // Método para cerrar sesión (opcional)
+  logout() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+    this.router.navigate(['/login']);
+  }
+}
