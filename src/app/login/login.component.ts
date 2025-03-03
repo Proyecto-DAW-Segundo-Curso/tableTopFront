a1acbeadf2a97aa4d6590316bc281fabb5414de7
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-03-03 17:55:53 +0100

    Actualizar el formulario de inicio de sesión con nuevos estilos y validaciones. Terminado

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { SpringAuthService } from '../spring-auth.service';
 
 @Component({
   selector: 'app-login',
@@ -17,28 +18,50 @@ export class LoginComponent {
   loginForm: FormGroup;
   errorMessage: string = '';
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  constructor(private fb: FormBuilder, private authService: SpringAuthService, private router: Router) {
     // Inicializa el formulario reactivo
     this.loginForm = this.fb.group({
       email: ['user@gmail.com', Validators.required],
-      password: ['user1234', Validators.required]
+      password: ['user1234', Validators.required,]
     })
   }
 
   // Método para manejar el envío del formulario
   onSubmit() {
     if (this.loginForm.valid) {
+      // Codigo DeepSeck
       const email = this.loginForm.get('email')?.value;
       const password = this.loginForm.get('password')?.value;
 
+      // const { email, password } = this.loginForm.value;
+
+      // Codigo DeepSeck
       // Intenta autenticar al usuario
-      if (this.authService.login(email, password)) {
-        this.router.navigate(['/dashboard']); // Redirige al dashboard
-      } else {
-        this.errorMessage = 'Email o contraseña incorrectos';
+      //   if (this.authService.login(email, password)) {
+      //     //this.router.navigate(['/dashboard']); // Redirige al dashboard
+      //     console.log('Usuario autenticado');
+      //     this.authService.isAuthenticated();
+
+      //   } else {
+      //     this.errorMessage = 'Email o contraseña incorrectos';
+      //   }
+      // } else {
+      //   this.errorMessage = 'Por favor, completa el formulario correctamente';
+      if (!email || !password) {
+        this.errorMessage = 'Por favor, ingresa tu correo y contraseña.';
+        return;
       }
-    } else {
-      this.errorMessage = 'Por favor, completa el formulario correctamente';
+
+      this.authService.login(email, password).subscribe({
+        next: () => {
+          this.router.navigate(['/dashboard']);
+        },
+        error: (err) => {
+          this.errorMessage = err.message || 'Error en la autenticación.';
+          console.error('Error en el login:', err);
+        }
+      })
+
     }
   }
 }
