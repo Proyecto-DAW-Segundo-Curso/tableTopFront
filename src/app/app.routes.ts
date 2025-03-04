b6cd5640b46eced2341f91be7c78f93a565188ec
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-03-04 14:00:41 +0100

    Agregar configuración de rutas y proveedores HTTP, y mejorar estilos de la tarjeta de inicio de sesión

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+// import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateEventComponent } from './create-event/create-event.component';
+import { EditEventComponent } from './edit-event/edit-event.component';
+import { ParticipantsComponent } from './participants/participants.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: '', component: AppComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'create-event', component: CreateEventComponent },
+    { path: 'edit-event', component: EditEventComponent },
+    { path: 'participants-list', component: ParticipantsComponent}
+];
