b9a7dbc1aaa95e22ffee1eecf26b4336eff91300
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-03-04 13:58:15 +0100

    Agregar formulario de registro con validaciones y manejo de errores

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SpringAuthService } from '../spring-auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  errorMessage: string = '';
 
+  constructor(private fb: FormBuilder, private router: Router, private authService: SpringAuthService) {
+    // Inicializa el formulario reactivo
+    this.registerForm = this.fb.group({
+      userName: ['user', ],
+      email: ['user@gmail.com', ],
+      password: ['user1234', ],
+    })
+  }
+
+  // Método para manejar el envío del formulario
+  onSubmit() {
+
+  }
 }
