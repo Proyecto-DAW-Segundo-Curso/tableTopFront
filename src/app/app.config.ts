20e48c46f1315847cfb8c0e226c2baadcfb8a0e3
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-03-06 22:06:59 +0100

    Agregar configuración de Firebase y archivo de entorno

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
-    provideHttpClient()]  
+    provideHttpClient(),
+    provideFirebaseApp(() =>
+      initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]  
 };
