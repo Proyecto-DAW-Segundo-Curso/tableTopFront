f39f25d6927c80aef0e0f7e272317208bd5deed4
Yulian Borges Lobaina <yborges2005@gmail.com>
2025-03-07 19:58:54 +0100

    Agregar interfaz de evento y servicio para obtener todos los eventos

---
+++ src/app/event.ts
@@ -0,0 +1,8 @@
+export interface Event {
+    id?: any;
+    date_time?: any;
+    location?: any;
+    max_players?: Number;
+    name?: string;
+    creator_id?: string;
+}
--- src/app/events.service.ts
+++ src/app/events.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor() { }
+  baseUrl = 'http://localhost:8080/api/events';
+
+  constructor( private http: HttpClient) { }
+
+  getAllEvents(): Observable<Event[]> {
+    return this.http.get<Event[]>(this.baseUrl);
+  }
 }
